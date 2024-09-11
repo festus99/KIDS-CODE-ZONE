5d2d1fe2731c2038450e1fb8243ecd91a5d822c8
festus99 <agyeifestus79@gmail.com>
2024-09-11 06:21:11 -0700

    first commit

---
+++ basic.py
@@ -0,0 +1,18 @@
+print(2 +2)
+# VARIABLE  NAMING
+my_students= [99,2, 4, 5,5 ,5]
+print(my_students[0])
+print(my_students[1])
+print( my_students[2])
+print(my_students[1])
+print(my_students[0])
+
+# PROGRAMMING COMMNETS
+
+#DATA TYPES 
+basic_eight = "kofi is a boy"
+basic_one = 'kofi is a boy'
+print(type(basic_eight))
+
+students = [ "chantel", "Adepa", "Noble","Samuel"]
+print(type(students))
